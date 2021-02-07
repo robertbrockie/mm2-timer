6e9782d04d6a57e549749a01dabeb9d18c7e5230
Robert Brockie <robertbrockie@gmail.com>
2021-02-07 15:02:00 -0500

    Clean up.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div></div>
-  );
+    return (
+        <div className="container">
+            <h1>Mega Man Timer</h1>
+        </div>
+    );
 }
 
 export default App;
